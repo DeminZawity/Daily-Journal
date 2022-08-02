0bc112baedb3aa5f970002306faa10593fa9cb14
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-08-02 14:44:23 -0500

    adding js files

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const journalEntries = [
+  {
+    id: 1,
+    date: "07/05/2022",
+    concept: "Orientation",
+    entry: "Today was the first day of my NSS bootcamp. We had orientation today and got to know everyone. Everyone seems really nice and sweet! I am very excited to get started!",
+    mood: "Happy",
+  },
+  {
+    id: 2,
+    date: "07/12/2022",
+    concept: "Tools",
+    entry: "This week I met with my team to go over certain tools like Github and Terminal. I am having a hard time understanding them BUT I am VERY determined to figure them out!",
+    mood: "Fine",
+  },
+  {
+    id: 3,
+    date: "07/19/2022",
+    concept: "Yin Yang Pt. 1",
+    entry:
+      "Today I met with my team to complete the first part of our Yin Yang project! Mariana designed our home page, and, of course, it looks amazing! I am done with my page, but I think it still needs some modifications to look better.",
+    mood: "Happy",
+  },
+  {
+    id: 4,
+    date: "07/26/2022",
+    concept: "Yin Yang Pt. 2",
+    entry: "I got my website to look SO much better! This week, Mariana and I decided to tackle the home page. I added a Nav Bar and she added a Footer. Our website is looking awesome!",
+    mood: "Happy",
+  },
+];
